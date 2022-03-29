6421731301902a28c8c0fb4a794ca8717b16a886
yukiyoyoyoyo <yukiyo2294@gmail.com>
2022-03-29 16:33:20 +0900

    物件object、web object事件處理程序設定、form表單

---
+++ script-object.js
@@ -0,0 +1,15 @@
+//第一種寫法：
+var person = {
+    name:"寶寶",
+    age:23,
+    sex:"女"
+};
+
+document.write(person.name);
+
+//第二種寫法：
+var colors = new Object();
+colors["white"]="白色";
+colors["red"]="紅色";
+colors["yellow"]="黃色";
+document.write(colors["red"]);
